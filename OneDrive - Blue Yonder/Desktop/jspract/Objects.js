ee9e30bb701ba45004c89228fe3681106f6b0fe9
Jayanthvedala <vedalajayanth@gmail.com>
2024-05-23 18:13:54 +0530

    js practice

---
+++ OneDrive - Blue Yonder/Desktop/jspract/Objects.js
@@ -0,0 +1,16 @@
+//const d = new Date();
+//const date = d.toISOString();
+//console.log(date);
+//const d = new Date();
+//const d2 = d.setDate(d.getDate() + 50);
+//console.log(d2);
+//let text = "A,B,C,D"
+//text = text.replaceAll("Cats","Dogs");
+//text = text.replaceAll("cats","dogs");
+//let array = text.split("|");
+//console.log(array); 
+//const d = new Date("October 13, 2014 11:13:00");
+//console.log(d);
+let d = new Date();
+d.setHours(22);
+console.log("updated time",d);
---
+++ OneDrive - Blue Yonder/Desktop/jspract/var.js
@@ -0,0 +1,16 @@
+let x = [23, 56, 11 , 72, 45, 88, 37, 90, 64, 29];
+
+let evennumber = [];
+let oddnumber = [];
+
+for(let i=0;i<x.length; i++){
+  if (x[i] % 2 === 0){
+    evennumber.push(x[i])
+
+  }else {
+    oddnumber.push(x[i])
+  }
+
+  }
+console.log(evennumber);
+console.log(oddnumber);t
